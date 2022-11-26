d4db543870fd3cb79c544419ae0b816067769e24
humamchoudhary <70004653+humamchoudhary@users.noreply.github.com>
2022-11-26 18:25:32 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import PortFolioDetails from './components/PortFolioDetails/PortFolioDetails';
-
+import Error404 from './components/404/404';
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/Portfolio/Portfolio"  element={<PortFolioDetails />} />
           <Route path='/Portfolio' element={<Home />} />
+          <Route path='/Portfolio/*' element={<Error404 />}/>
         </Routes>
 
       </Router>
